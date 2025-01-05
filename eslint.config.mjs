f861cef55603546f9144adfddddb8c07913d9f33
md1300 <md.tawhid.khan1998@gmail.com>
2025-01-05 12:25:44 +0600

    gitIgnore add

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,12 @@ export default [
   {
     rules: {
       'no-unused-vars': 'error',
+      'no-unused-expressions':'error',
+      'prefer-const':'error',     
+      'no-console':'warn',
+      'no-undef':'error',
     },
+    
   },
   {
     ignores: ['.node_modules/*', 'dist'],
